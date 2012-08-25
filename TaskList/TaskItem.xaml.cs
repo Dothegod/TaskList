28288d3ed80d3cc8c34f01eb2e014b981b15d77f
Liang Wei <dothegod@gmail.com>
2012-08-25 23:15:53 +0800

    project create

---
+++ TaskList/TaskItem.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskList
+{
+    /// <summary>
+    /// Interaction logic for TaskItem.xaml
+    /// </summary>
+    public partial class TaskItem : UserControl
+    {
+        public TaskItem(DelTaskItem Deletefun)
+        {
+            InitializeComponent();
+            this.DeleteSelf = Deletefun;
+        }
+
+        private void TextBox_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            TaskContent.IsReadOnly = false;            
+        }
+
+        private void TaskContent_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TaskContent.IsReadOnly = true;
+            }
+        }
+
+        public delegate void DelTaskItem(UIElement sender);
+        public DelTaskItem DeleteSelf = null;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (DeleteSelf != null)
+            {
+                DeleteSelf((UIElement)this);
+            }
+                        
+        }
+
+        private void CheckBox_Click_1(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            TaskContent.IsEnabled = !((bool)cb.IsChecked);
+        }
+    }
+}
---
+++ TaskList/TaskShell.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskList
+{
+    /// <summary>
+    /// Interaction logic for TaskShell.xaml
+    /// </summary>
+    public partial class TaskShell : UserControl
+    {
+        public TaskShell()
+        {
+            InitializeComponent();
+            AddTaskItem();
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            AddTaskItem();
+        }
+
+        private void AddTaskItem()
+        {
+            Pool.Children.Add(new TaskItem(DelTask));
+        }
+
+        private void DelTask(UIElement item)
+        {
+            Pool.Children.Remove(item);
+        }
+    }
+}
